1bb3e9a9533e1628b7409d7227109c583938fe47
yura50525 <68415094+yura50525@users.noreply.github.com>
2023-05-04 16:15:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,48 +1,56 @@
+function getQuestionAnswer(apiKey, question, answers) {
+  const apiUrl = "https://api.openai.com/v1/chat/completions";
 
-function getMultiQuestionAnswer(apiKey, question, answers){
-  const apiUrl = 'https://api.openai.com/v1/chat/completions';
-
-fetch(apiUrl, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${apiKey}`
-  },
-  body: JSON.stringify({
-    model: 'gpt-3.5-turbo',
-    messages: [{role: 'user', content: `Дай відповідь на запитання вибравши 1 відповідь: "${question}"Варіанти відповіді[${answers}]`}]
+  fetch(apiUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: `Дай відповідь на запитання вибравши 1 відповідь: "${question}"Варіанти відповіді[${answers}]`,
+        },
+      ],
+    }),
   })
-})
-.then(response => response.json())
-.then(data => {
-  console.log(data.choices[0].message.content)
-  var fourthDiv = document.querySelector('#test7429573 > div > div:nth-child(2) > div > div > div.v-test-go-body-test-button');
-  var newParagraph = document.createElement('p');
-newParagraph.textContent = data.choices[0].message.content;
+    .then((response) => response.json())
+    .then((data) => {
+      const generetedAns = data.choices[0].message.content;
+      var divForBtn = document.querySelector(".v-test-go-body-test-button");
+      var ansTextarea = document.createElement("p");
+      ansTextarea.textContent = generetedAns;
 
-fourthDiv.appendChild(newParagraph);
+      divForBtn.appendChild(ansTextarea);
+    })
+    .catch((error) => {
+      console.error(error);
+      var divForBtn = document.querySelector(".v-test-go-body-test-button");
+      var ansTextarea = document.createElement("p");
+      ansTextarea.textContent = "error";
 
-})
-.catch(error => console.error(error));
-
+      divForBtn.appendChild(ansTextarea);
+    });
 }
 
+function startBot(apiKey) {
+  var mainBtn = document.querySelector(".v-icon-timer-new");
 
-function startBot(){
-  var button = document.querySelector('.v-icon-timer-new');
+  // Add an onclick event listener to the button
+  mainBtn.addEventListener("click", function () {
+    const question = document.querySelector(".content-box").textContent;
+    const divWithAns = document.querySelector(".flex-row-test");
+    const labelsWithAns = divWithAns.querySelectorAll("label");
 
-// Add an onclick event listener to the button
-button.addEventListener('click', function() {
-  const vQuestion= document.querySelector('#i-test-question-jbw350 > div > div.v-test-questions-title').textContent;
-const divWithAnswers = document.querySelector('#i-test-question-jbw350 > div > div:nth-child(2) > div');
-const ansversText = divWithAnswers.querySelectorAll('label');
-
-var vAnsversString = '';
-ansversText.forEach(function(label) {
-  vAnsversString = vAnsversString + `"${label.textContent}",`;
-})
-console.log(vQuestion)
-console.log(vAnsversString)
-getMultiQuestionAnswer('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', vQuestion, vAnsversString);
-});
+    var ansString = "";
+    labelsWithAns.forEach(function (label) {
+      ansString = ansString + `"${label.textContent}",`;
+    });
+    console.log(question);
+    console.log(ansString);
+    getQuestionAnswer(apiKey, question, ansString);
+  });
 }
